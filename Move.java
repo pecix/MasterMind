128a28679bda06e092457eb9c4cafd629d133758
pecix <pecixx@gmail.com>
2018-11-03 20:32:11 +0100

    Master Mind

---
+++ Move.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static Playground.mastermind.Code.CODE_SIZE;
+
+public class Move {
+
+    private Code password;
+    private Code guess;
+
+    public Move(Code password, Code guess) {
+        this.password = password;
+        this.guess = guess;
+    }
+
+    public boolean isGuessed() {
+        return password.equals(guess);
+    }
+
+    public int numberGuessed() {
+        int count = 0;
+
+//        for (int i=0; i<CODE_SIZE; i++){
+//            if(password.getCode().charAt(i) == guess.getCode().charAt(i)){
+//                count++;
+//            }
+//        }
+
+        count = (int) Stream.iterate(0, n -> n + 1)
+                .limit(CODE_SIZE)
+                .filter(i -> password.getCode().charAt(i) == guess.getCode().charAt(i))
+                .count();
+
+        return count;
+    }
+
+    public int numberMissed() {
+        int count = 0;
+
+        String passwordCode = password.getCode();
+        String guessCode = guess.getCode();
+
+        List<Character> passwordDiff = new ArrayList<>();
+        List<Character> guessDiff = new ArrayList<>();
+
+
+        for (int i = 0; i < CODE_SIZE; i++) {
+            if (passwordCode.charAt(i) != guessCode.charAt(i)) {
+                passwordDiff.add(passwordCode.charAt(i));
+                guessDiff.add(guessCode.charAt(i));
+            }
+        }
+
+        for (Character ch : guessDiff) {
+            if (passwordDiff.contains(ch)) {
+                count++;
+                passwordDiff.remove(ch);
+            }
+        }
+
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, OK: %d, MISS: %d", guess.getCode(), numberGuessed(), numberMissed());
+    }
+}
